9ff632f2ff740fc5bb5184fe309aa591980c9087
Brett Roberts <brett.roberts@live.ca>
2023-09-18 19:26:31 -0600

    Create two queries to find students total assignment duration and cohorts total assignment duration

---
+++ 2_queries_joins/2_total_cohort_assignment_time.sql
@@ -0,0 +1,6 @@
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+INNER JOIN students ON student_id = students.id
+INNER JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE 'FEB12';
+
